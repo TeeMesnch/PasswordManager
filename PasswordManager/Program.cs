40fa2f1cd9a0c321125ae96475cfb1c26d49b62b
TeeMensch <jonathanasmussen@gmail.com>
2025-10-18 16:51:46 +0200

    initial commit

---
+++ PasswordManager/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace PasswordManager
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var commandList = new Dictionary<string, Action>
+            {
+                { "INIT", Init.Initialize},
+                { "--add", CommandExecution.Add },
+                { "-A", CommandExecution.Add },
+                { "--remove", CommandExecution.Remove },
+                { "-R", CommandExecution.Remove },
+                { "--change", CommandExecution.Change },
+                { "-C", CommandExecution.Change },
+                { "--show", CommandExecution.Show },
+                { "-S", CommandExecution.Show },
+                { "--help", CommandExecution.Help },
+                { "-H", CommandExecution.Help }
+            };
+
+            if (commandList.TryGetValue(args[0], out var commandAction))
+            {
+                commandAction();
+            }
+            else
+            {
+                Console.WriteLine("Unknown command try --help or -H");
+            }
+        }
+    }
+
+    static class Init
+    {
+        public static void Initialize()
+        {
+            
+        }
+    }
+
+    static class CommandExecution
+    {
+        public static void Add()
+        {
+            
+        }
+
+        public static void Remove()
+        {
+            
+        }
+
+        public static void Change()
+        {
+            
+        }
+
+        public static void Show()
+        {
+            
+        }
+
+        public static void Help()
+        {
+            
+        }
+    }
+}
